34e44685648e9d3716ab37e009e4ca578055486e
Ggaik1 <gagik.asatryan.v@gmail.com>
2023-03-31 16:14:03 +0400

    i for lessons

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function MyCoolFunc(silvik, seda) {
+//return 'uborka em arel'
+//+ silvik, + ' ev ' + seda;
+//}
+
+// function xoranard (erkarutyun, laynutyun, bardzrutyun){
+   // return erkarutyun * laynutyun * bardzrutyun;
+//};
+//console.log (xoranard (4,10,900));
+
+
+function xoranard (erkarutyun, laynutyun, bardzrutyun) {
+    return erkarutyun * laynutyun * bardzrutyun;
+};
+console.log (xoranard (1000000000004546362,2352385728395,29375235))
---
+++ task2.js
@@ -0,0 +1,73 @@
+// Task 1
+// create a function, that will get a number as a parameter and the square root of that number 
+// (4) will return 16, (10) will retun 100
+
+// Task 2
+persons = [
+  {
+    id: 325326,
+    firstname: "Dan",
+    lastname: "Tiger",
+    image: "url",
+    birthday: "03.18.1990",
+    gender: "Male",
+    hobbie: ["Football", "Reading"],
+    nationality: "switzerland",
+    friends: [325330, 325334],
+    residence: 10041
+  },
+  {
+    id: 325326,
+    firstname: "Fan",
+    lastname: "Tiger",
+    image: "url",
+    birthday: "03.18.2005",
+    gender: "Male",
+    hobbie: ["Football", "Reading"],
+    nationality: "switzerland",
+    friends: [325330, 325334],
+    residence: 10041
+  },
+  {
+    id: 325330,
+    firstname: "Frank",
+    lastname: "Lion",
+    image: "url",
+    birthday: "10.05.1991",
+    gender: "male",
+    hobbie: ["walking", "basketball"],
+    nationality: "syria",
+    friends: ["", "", ""],
+    residence: 10041
+  },
+{
+    id: 325334,
+    firstname: "Bilal",
+    lastname: "Zakki",
+    image: "url",
+    birthday: "08.19.1999",
+    gender: "male",
+    hobbie: ["swimming", "walking"],
+    nationality: "canda",
+    friends: [325330, 325326],
+    residence: 10012
+  }
+];
+
+ // create a function that will get any of the items from persons array as a parameter and return the 
+// following string 'I have just met a person whose name is <firstname> + <lastname>';
+//let string = 'I have just met a person whose name is ';
+//function sentence() {
+  //return string + persons[1].firstname + " " + persons[1].lastname;
+//};
+//console.log(sentence());
+
+
+//let string = ' is from '
+
+//function Human() {
+  //return persons[2].firstname + ' ' + persons[2].lastname + ' is from ' + persons[3].nationality;
+//};
+//console.log(Human());
+
+let v
